6bc144efe7eec52cc3ae1ae3ef4ac4385d963ad5
Diego F. Aranha <dfaranha@gmail.com>
2023-09-27 18:10:09 +0200

    Enable assertions.

--- script/run.sh
+++ script/run.sh
@@ -5,7 +5,7 @@ set -x
 #touch ../build && rm ../build -rf
 mkdir -p ../build
 cd ../build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 
 ./src/virgo_plus_run ../data/SHA256_64.pws
